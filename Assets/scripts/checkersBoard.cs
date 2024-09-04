30965b706d326bafef51cba3f8b95062ce810b37
andree134 <ap6850k@gre.ac.uk>
2024-09-04 19:51:27 +0100

    board changed, not functional

--- Assets/scripts/checkersBoard.cs
+++ Assets/scripts/checkersBoard.cs
@@ -4,15 +4,126 @@ using UnityEngine;
 
 public class checkersBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Piece[,] pieces = new Piece[8, 8];
+    public GameObject whitePiecePrefab;
+    public GameObject blackPiecePrefab;
+
+    private Vector3 boardOffset = new Vector3(-4.0f, 0, -4.0f);  //offset for pieces to be on board
+    private Vector3 pieceOffset = new Vector3(0.5f, 0, 0.5f);   //offset for pieces to match boxes
+
+    private Piece selectedPiece;
+
+    private Vector2 mouseOver; // mouse pos
+    private Vector3 startDrag; //will drag the pieces instead of point and click, might change later
+    private Vector3 endDrag;
+
+    private void Start()
     {
-        
+        GenerateBoard();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateMouseOver();
+        //Debug.Log(mouseOver); //board collider -0.08 from edges, change when replacing asset
+
+        //if player turn
+        {
+            int x = (int)mouseOver.x;
+            int y = (int)mouseOver.y;
+
+            if (Input.GetMouseButtonDown(0)) 
+                SelectPiece(x,y);
+        }
+    }
+     
+    private void UpdateMouseOver()
+    {
+        //if player turn
+        if (!Camera.main)
+        {
+            Debug.Log("No main camera found");
+            return;
+        }
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board"))) //if the mouse is on board
+        {
+            mouseOver.x = (int)hit.point.x - boardOffset.x; //int so it snaps to a decimal point
+            mouseOver.y = (int)hit.point.z - boardOffset.z; // on z since board is on floor not wall
+        }
+        else
+        {
+            mouseOver.x = -1;
+            mouseOver.y = -1;
+        }
+    }
+
+    private void SelectPiece(int x, int y)
+    {
+        //out of bounds
+        if ( x < 0 || x >= 8 || y < 0 || y >= 8) //RAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHH
+            return;
+
+        Piece p = pieces[x, y];
+        if(p != null)
+        {
+            selectedPiece = p;
+            startDrag = mouseOver;
+            Debug.Log(selectedPiece.name);
+        }
+    }
+
+    private void GenerateBoard()
+    {
+        //generate white  team
+        for (int y = 0; y<3; y++)
+        {
+            bool oddRow = (y % 2 == 0);
+            for(int x = 0; x<8; x+= 2)
+            {
+                if (oddRow)
+                {
+                    GeneratePiece(x, y);
+                }
+                else
+                {
+                    GeneratePiece(x+1, y);
+                }
+                
+            }
+        }
+
+        //generate black team
+        for (int y = 7; y > 4; y--)
+        {
+            bool oddRow = (y % 2 == 0);
+            for (int x = 0; x < 8; x += 2)
+            {
+                if (oddRow)
+                {
+                    GeneratePiece(x, y);
+                }
+                else
+                {
+                    GeneratePiece(x +1, y);
+                }
+
+            }
+        }
+    }
+
+    private void GeneratePiece(int x, int y)
+    {
+        bool isPieceWhite = (y > 3) ? false : true; //if y>3 = false, else true;
+        GameObject gopiece = Instantiate((isPieceWhite) ? whitePiecePrefab : blackPiecePrefab) as GameObject; //spawn whitePiece if false, else blackPiece
+        gopiece.transform.SetParent(transform);
+        Piece p = gopiece.GetComponent<Piece>();
+        pieces[x, y] = p;
+        MovePiece(p,x,y);
+    }
+
+    private void MovePiece(Piece p, int x, int y)
+    {
+        p.transform.position = (Vector3.right * x) + (Vector3.forward * y) + boardOffset + pieceOffset;
     }
 }
